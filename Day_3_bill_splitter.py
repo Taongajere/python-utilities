2bef2ca31c97050b7bd132b24810501dab6de14b
Taongajere <jeretaonga.tj@gmail.com>
2025-10-03 09:21:00 +0200

    first commit

---
+++ Day_3_bill_splitter.py
@@ -0,0 +1,13 @@
+"""
+ Challenge: Simple Bill Splitter
+
+Write a Python script that helps split a bill evenly between friends.
+
+Your program should:
+1. Ask how many people are in the group.
+2. Ask for each person's name.
+3. Ask for the total bill amount.
+4. Calculate each person's share of the bill.
+5. Display how much each person owes in a clean, readable format.
+"""
+
